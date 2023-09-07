038ba917ee511d78fb26115c948e98052b3a9a0d
SeokJeong0411 <dk04188@naver.com>
2023-09-07 21:10:16 +0900

    select with condition

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,7 +6,7 @@ import { v4 as uuid } from "uuid";
 const Home = ({ auth }) => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
-  const [attachment, setAttachment] = useState();
+  const [attachment, setAttachment] = useState("");
   const db = dbService.getFirestore();
 
   const getNweets = async () => {
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,35 @@
-import React from "react";
-import { authService } from "fbase";
+import React, { useEffect } from "react";
+import { authService, dbService } from "fbase";
 import { useNavigate } from "react-router-dom";
 
 const Profile = ({ auth }) => {
   const navigate = useNavigate();
+  const db = dbService.getFirestore();
+
   const onLogOutClick = () => {
     authService.signOut(auth);
     navigate("/");
   };
+  const getMyNweets = async () => {
+    const q = dbService.query(
+      dbService.collection(db, "nweets"),
+      dbService.where("creatorId", "==", auth.currentUser.uid)
+    );
+    const querySnapshot = await dbService.getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      const nweetObject = {
+        ...doc.data(),
+        id: doc.id,
+      };
+      console.log(nweetObject);
+      // setNweets((prev) => [nweetObject, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getMyNweets();
+  }, []);
 
   return (
     <>
